b7ab5d04157082207c45a38dcd94f5d6264d2fdd
Ederson Viana <edersonvangeli@gmail.com>
2025-05-26 12:04:40 -0300

    adicionados guards para carros

--- src/modules/carro/carro.controller.ts
+++ src/modules/carro/carro.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { CarroService } from './carro.service';
 import { CreateCarroDto } from './dtos/create-carro.dto';
 import { UpdateCarroDto } from './dtos/update-carro.dto';
+import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('carros')
 export class CarroController {
-  constructor(private readonly carroService: CarroService) {}
+  constructor(private readonly carroService: CarroService) { }
 
   @Post()
+  @Roles('ADMIN')
   create(@Body() createCarroDto: CreateCarroDto) {
     return this.carroService.create(createCarroDto);
   }
@@ -23,11 +28,13 @@ export class CarroController {
   }
 
   @Put(':id')
+  @Roles('ADMIN') 
   update(@Param('id') id: string, @Body() updateCarroDto: Partial<UpdateCarroDto>) {
     return this.carroService.update(id, updateCarroDto);
   }
 
   @Delete(':id')
+  @Roles('ADMIN') 
   remove(@Param('id') id: string) {
     return this.carroService.remove(id);
   }
